91413e6e7250515f131b57bb3f04cc69dfd5861a
Venny <sgtmattis@gmail.com>
2020-08-04 17:49:46 +0200

    Add Twitter Oauth handler

--- Vensha/Handlers/TwitterHandler.cs
+++ Vensha/Handlers/TwitterHandler.cs
@@ -1,4 +1,7 @@
 using CoreTweet;
+using System;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace Vensha.Handlers
 {
@@ -8,12 +11,34 @@ namespace Vensha.Handlers
 
         public TwitterHandler(Types.Configuration config)
         {
-            client = Tokens.Create(
-                config.twitter.consumer_key,
-                config.twitter.consumer_secret,
-                config.twitter.access_token,
-                config.twitter.access_token_secret
-            );
+
+            if (String.IsNullOrEmpty(config.twitter.access_token))
+            {
+                var session = OAuth.Authorize(config.twitter.consumer_key, config.twitter.consumer_secret);
+
+                Console.WriteLine($"Looks like this is your first time using this application! Pleae open the following link and authorise, then paste the obtained token here: {session.AuthorizeUri.AbsoluteUri}");
+                string token = Console.ReadLine();
+
+                try
+                {
+
+                    client = OAuth.GetTokens(session, token);
+                    config.twitter.access_token = client.AccessToken;
+                    config.twitter.access_token_secret = client.AccessTokenSecret;
+                    System.IO.File.WriteAllText(Directory.GetCurrentDirectory() + "/config.json", JsonConvert.SerializeObject(config, Formatting.Indented));
+                }
+                catch
+                {
+                    Console.WriteLine("That was not a valid token!");
+                    Environment.Exit(1);
+                }
+            }
+            else
+            {
+                client = Tokens.Create(config.twitter.consumer_key, config.twitter.consumer_secret, config.twitter.access_token, config.twitter.access_token_secret);
+            }
+
+            client.Statuses.Update(new { status = "Test" + new Random().Next(1, 100) });
         }
     }
 }
